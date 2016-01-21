12bfde01e9d2d34d2675aa7852e2a7c4ae761fca
mlxd <loriordan@gmail.com>
2016-01-21 16:56:03 +0900

    Updated psi6 to precalc values for performance

---
+++ binData.m
@@ -0,0 +1,13 @@
+function [g6B,bin] = binData(g6C,bins)
+bin = linspace(0,max(g6C(1,:)),bins);
+g6B = zeros(length(bin)-1,1);
+for kk=1:(length(bin)-1)
+    idx = find( g6C(1,:) >= bin(kk) & g6C(1,:) < bin(kk+1) );
+    if length(idx) >0
+        g6B(kk) = sum(g6C(2,idx))./length(idx);
+    else
+        g6B(kk) = 0;
+    end
+end
+
+end
--- g6_struct.m
+++ g6_struct.m
@@ -1,12 +1,16 @@
 function [g6C] = g6_struct(X,Y)
-S = uniqPairIdx([X,Y]);
+for ii=1:length(X)
+    psi6p(ii) = psi6([X(ii),Y(ii)],X,Y,35);
+end
+
+S = uniqPairIdx_precalc([X,Y],psi6p);
 [~,order] = sort([S(:).rabs],'ascend');
 SSorted = S(order);
 
 for ii=1:size(SSorted,2)
     g6S(ii).r = SSorted(ii).rabs;
-    psi6_1 = (psi6([SSorted(ii).pos0(1),SSorted(ii).pos0(2)],X,Y,50));
-    psi6_2 = (psi6([SSorted(ii).pos1(1),SSorted(ii).pos1(2)],X,Y,50));
+    psi6_1 = SSorted(ii).cor0;
+    psi6_2 = SSorted(ii).cor1;
     g6S(ii).val = abs(conj(psi6_1)*conj(psi6_1));
 end
 g6C = cell2mat(squeeze(struct2cell(g6S)));
